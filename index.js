9c777b2c77a26251c455589812be58edc30054e7
Armand72 <armandmeunier@hotmail.fr>
2020-07-18 12:08:22 +0200

    seperated nodemailer

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const parser = require("body-parser");
 const app = express();
 const cors = require("cors");
-const nodemailer = require("nodemailer");
+const api = require("./routes/sendMail");
+
 var path = require("path");
 require("dotenv").config();
 const SERVER_ADDRESS = process.env.SERVER_ADDRESS;
-const EMAIL = process.env.EMAIL;
-const PASSWORD = process.env.PASSWORD;
 
 app.use(parser.json());
 app.use(
@@ -17,47 +16,9 @@ app.use(
 );
 app.use(cors());
 
+app.use("", api);
+
 app.use(express.static(path.join(__dirname, "client/build")));
-
-const transporter = nodemailer.createTransport({
-  host: "smtp-mail.outlook.com", // hostname
-  secureConnection: false,
-
-  port: 587, // port for secure SMTP
-  auth: {
-    user: EMAIL,
-    pass: PASSWORD,
-  },
-  tls: {
-    ciphers: "SSLv3",
-  },
-});
-
-app.post("/sendmail", (req, res) => {
-  const user = req.body;
-
-  const htmlMessage = `<h1>nom:${user.fullname}  </h1>
-  <h2>adresse email : ${user.email}</h2>
-  <h2>Numéro de téléphone: ${user.phonenumber}</h2>
-	<p>${user.message}</p>`;
-
-  let mailOptions = {
-    from: EMAIL,
-    to: EMAIL,
-    subject: "Message client",
-    text: "",
-    html: htmlMessage,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    console.log("here", error, info);
-    if (error) {
-      return res.status(501).json(error);
-    }
-
-    res.sendStatus(200);
-  });
-});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
---
+++ routes/sendMail.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const EMAIL = process.env.EMAIL;
+const PASSWORD = process.env.PASSWORD;
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com", // hostname
+  secureConnection: false,
+
+  port: 587, // port for secure SMTP
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD,
+  },
+  tls: {
+    ciphers: "SSLv3",
+  },
+});
+
+router.post("/sendmail", (req, res) => {
+  const user = req.body;
+
+  const htmlMessage = `<h1>nom:${user.fullname}  </h1>
+    <h2>adresse email : ${user.email}</h2>
+    <h2>Numéro de téléphone: ${user.phonenumber}</h2>
+      <p>${user.message}</p>`;
+
+  let mailOptions = {
+    from: EMAIL,
+    to: EMAIL,
+    subject: "Message client",
+    text: "",
+    html: htmlMessage,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    console.log("here", error, info);
+    if (error) {
+      return res.status(501).json(error);
+    }
+
+    res.sendStatus(200);
+  });
+});
+
+module.exports = router;
