1d967ece672515e966f2334d6dfb62f830038b87
Armand72 <52382041+Armand72@users.noreply.github.com>
2020-07-20 12:00:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.use("", api);
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(expressStaticGzip(path.join(__dirname, "client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
