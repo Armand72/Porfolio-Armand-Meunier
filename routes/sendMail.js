a07b4b931523bb820211c169b32a03b21d98ea3d
Armand72 <armandmeunier@hotmail.fr>
2020-08-22 19:31:14 +0200

    0Auth

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const nodemailer = require("nodemailer");
+const OAuth2 = google.auth.OAuth2;
 require("dotenv").config();
 
 const EMAIL = process.env.EMAIL;
 const PASSWORD = process.env.PASSWORD;
 
+const myOAuth2Client = new OAuth2(
+  "1042211473852-g1ejd8ecl2bj26gs2b6ldiau4usj7moc.apps.googleusercontent.com",
+  "ec7y4jSkXXzFDtfDFIrne1vg"
+);
+
+myOAuth2Client.setCredentials({
+  refresh_token:
+    "1//04evVwqKrCA-gCgYIARAAGAQSNwF-L9IrxS_dJASZGZdzaNQkVBY6cdaOBK7n3j3nfnvQh4dQWKlPF3IzuIOeJLmR-_oV-wP7lcE",
+});
+
+const myAccessToken = oauth2Client.getAccessToken();
+
 const transporter = nodemailer.createTransport({
-  name: "Gmail",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
-    user: "armandmeunierdeveloper@gmail.com",
-    pass: "CogitoErgoSum72%",
-  },
-  tls: {
-    rejectUnauthorized: false,
+    type: "OAuth2",
+    user: EMAIL, //your gmail account you used to set the project up in google cloud console"
+    clientId:
+      "1042211473852-g1ejd8ecl2bj26gs2b6ldiau4usj7moc.apps.googleusercontent.com",
+    clientSecret: "ec7y4jSkXXzFDtfDFIrne1vg",
+    refreshToken:
+      "1//04evVwqKrCA-gCgYIARAAGAQSNwF-L9IrxS_dJASZGZdzaNQkVBY6cdaOBK7n3j3nfnvQh4dQWKlPF3IzuIOeJLmR-_oV-wP7lcE",
+    accessToken:
+      "ya29.a0AfH6SMAxuNL75ykPXSCJD2x8e8Wh_qkjFQq-xrbAC48yn12eZv1QM4kcM2tvUb5k_LeDzI1tW5VHmlsG0iQSdUBo96ZF1G42fQnaOxVAQDMu2cX1zINk4Xs_OhNY0HkFpsmjRsvbZS3elbRANrH_ra9NQzTVthkDFAc",
   },
 });
 
